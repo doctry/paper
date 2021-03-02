3ab352a6baae4aa107597ffbd210c8e74b45a300
doctry <jonathanyang475869@gmail.com>
2021-03-02 23:41:04 +0800

    2dgaussian

---
+++ EUV double patterning/testing/2dGaussian.py
@@ -0,0 +1,13 @@
+import math
+import numpy as np
+
+width = 1000
+sigma = 500
+
+ret = np.zeros(shape=(2*width + 1, 2*width + 1))
+
+for i in range(-width,width + 1):
+    for j in range(-width, width + 1):
+        ret[i+width][j+width] = math.exp(-(i*i+j*j)/(2*sigma*sigma))
+
+np.save('2dGaussian.npy',ret)
